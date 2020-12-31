640d80d8e3a55d2a6bb166e24e0d2abe7620b9ff
Jason Beedle <jbeedle@gmail.com>
2020-12-31 16:30:42 +0000

    initial commit

---
+++ src/App.js
@@ -0,0 +1,73 @@
+import React, { usestate } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import * as Font from 'expo-font';
+import { AppLoading } from 'expo';
+
+const fetchFonts = () => {
+  return Font.loadAsync ({
+    'open-sans': require('assets/fonts/OpenSans-regular.ttf'),
+    'open-sans-bold': require('assets/fonts/OpenSans-Bold-regular.ttf'),
+  });
+}
+
+export default function App() {
+  const [userNumber, setUserNumber ] = userState();
+  const [guessRounds, setGuessRounds ] = userState(0);
+  const [dataLoaded, setDataLoaded ] = userState(false);
+
+  if(!dataLoaded) {
+    return(
+      <AppLoading
+        startAsync={fetchFonts}
+        onFinish={() => setDataLoaded (true)}
+        onError={(err) => console.log(err)}
+      />
+    );
+  }
+
+  function configureNewGameHandler() {
+    setUserNumber(selectedNumber);
+    setGuessRounds(0)
+    setUserNumber(null)
+  }
+
+  function startGameHandler(selectedNumber) {
+    setUserNumber(selectedNumber);
+    setGuessRounds(0)
+  }
+
+  function gameOverHandler(numOfRounds) {
+    setGuessRounds(numOfRounds)
+  }
+
+  let content = <StartGameScreen onStartGame={startGameHandler} />
+
+  if (userNumber && guessRounds <= 0) {
+    content = (
+      <GameScreen
+        userChoice={userNumber}
+        onGameOver={gameOverHandler}
+      />
+    );
+  } else if (guessRounds > 0 ) {
+    content = (
+      <GameOverScreen
+        roundsNumber={guessRounds}
+        userNumber={userNumber}
+        onRestart={configureNewGameHandler}
+      />
+    );
+  }
+
+  return (
+    <View style={StyleSheet.screen}>
+      <Header title="Guess A Number" />
+      {content}
+    </View>
+  );
+  const styles = StyleSheet.create({
+    screem: {
+      flex:1,
+    },
+  })
+   }
