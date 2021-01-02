611d0b86d72c53a4320f298d88c1d3ed562ace17
Jason Beedle <jbeedle@gmail.com>
2021-01-02 10:24:36 +0000

    added the card props

---
+++ src/Components/Card.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { NativeAppEventEmitter } from 'react-native';
+import { View, StyleSheet } from 'react-native';
+
+export default function Card(props ) {
+    return (
+        <View style={{ ...StyleSheet.card, ...props.style }}>{props.children}</View>
+    );
+}
+
+const styles = StyleSheet.create({
+    card: {
+      shadowColor: 'black',
+      shadowOffset: { width: 0, height: 2 },
+      shadowRadius: 6,
+      shadowOpacity: 0.26,
+      elevation: 5,
+      backgroundColor: 'white',
+      padding: 20,
+      borderRadius: 10,
+    },
+  }); 
